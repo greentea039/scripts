5116b5002de4a82d6dbda71427ff30b78898e310
greentea039 <lemontea053-eh@yahoo.co.jp>
2013-04-08 09:41:36 +0900

    Add the correction of Moogle Mail attachment display issue

--- HVLayoutCorrector.user.js
+++ HVLayoutCorrector.user.js
@@ -1,23 +1,32 @@
 // ==UserScript==
 // @name          HV Layout Corrector
 // @namespace     HV Layout Corrector
-// @version       1.1
+// @version       1.2
 // @description   Correct corrupted layouts
 // @include       http://hentaiverse.org/*
 // @exclude       http://hentaiverse.org/pages/showequip*
 // @run-at        document-end
 // ==/UserScript==
+
+var hv = {
+	settings: {
+		get useHVFontEngine() {
+			return document.getElementsByClassName('fd10')[0].textContent !== "Health points";
+		},
+	},
+};
+
 (function () {
 	var i, elements;
 
 	// Character
 	if (document.location.search.indexOf("?s=Character") >= 0) {
-		// avoid overlap
+		// Avoid overlap
 		elements = document.querySelectorAll('#stats_pane > div');
 		for (i = 0; i < elements.length; i++) {
 			elements[i].style.removeProperty("height");
 		}
-		// center horizontal position of 'Effective Proficiency'
+		// Center horizontal position of 'Effective Proficiency'
 		elements = document.querySelectorAll('#stats_pane div.eqsp1 > div.fd12 > div');
 		for (i = 0; i < elements.length; i++) {
 			elements[i].style.cssText += "text-align: center;";
@@ -32,7 +41,7 @@
 	}
 	// Arena entrance
 	if (document.location.search.indexOf("?s=Battle&ss=ar") >= 0) {
-		// avoid overlap
+		// Avoid overlap
 		elements = document.querySelectorAll('#arena_pane > div');
 		for (i = 0; i < elements.length; i++) {
 			elements[i].style.removeProperty("height");
@@ -40,11 +49,22 @@
 	}
 	// Ring of Blood entrance
 	if (document.location.search.indexOf("?s=Battle&ss=rb") >= 0) {
-		// reveal hidden message
+		// Reveal hidden message
 		elements = document.querySelectorAll('#arenaform > div');
 		for (i = 0; i < elements.length; i++) {
 			elements[i].style.removeProperty("float");
 			elements[i].style.removeProperty("overflow");
+		}
+	}
+	// Moogle Mail Write New
+	if (document.location.search.indexOf("?s=Bazaar&ss=mm&filter=Write%20New") >= 0) {
+		// Avoid attachment display overlap
+		if (!hv.settings.useHVFontEngine) {
+			elements = document.querySelectorAll('#mailform > div > div + div div');
+			for (i = 0; i < elements.length; i++) {
+				elements[i].style.removeProperty("width");
+				elements[i].style.display = "inline-block";
+			}
 		}
 	}
 })();
